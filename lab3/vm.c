04c64cab8338bd18e5ba3aa9f0a39522ad94fe02
Jacques Fracchia <jfrac001@sledge.cs.ucr.edu>
2019-11-13 20:38:42 -0800

    updated proc.c .h and vm.c

--- lab3/vm.c
+++ lab3/vm.c
@@ -337,6 +337,25 @@ copyuvm(pde_t *pgdir, uint sz)
       goto bad;
     }
   }
+  
+  //Lab3 begin
+  uint stackEnd = STACKTOP - PGSIZE*(myproc()->stackPages);
+  
+  for(i = PGROUNDUP(stackEnd); i < STACKTOP; i += PGSIZE){
+	if((pte = walkpgdir(pgdir, (void *) i, 0)) == 0)
+	    panic("copyuvm: pte should exist");
+	if(!(*pte & PTE_P))
+    	    panic("copyuvm: page not present");
+	pa = PTE_ADDR(*pte);
+	flags = PTE_FLAGS(*pte);
+	if((mem = kalloc()) == 0)
+	   goto bad;
+	memmove(mem, (char*)P2V(pa), PGSIZE);
+	if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0){
+	   kfree(mem);
+	   goto bad;
+	}
+  }
   return d;
 
 bad:
